a2b9264bd94741a11a7e67a6d08f5b787ed8cbed
Martin-Prusa <martin.prusa@educanet.cz>
2021-11-26 17:15:52 +0100

    withoutBonus

--- src/main/java/streams/DataProcessor.java
+++ src/main/java/streams/DataProcessor.java
@@ -3,42 +3,45 @@ package streams;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class DataProcessor {
 
 
     public static boolean atLeastOneGradeA(Student student) {
-        return false;
+        return student.getGrades().stream().anyMatch(i -> i.getType() == GradeType.A);
     }
 
 
     public static List<Integer> getStudentAges(List<Student> students) {
-        return new ArrayList<>();
+        return students.stream()
+                .map(i -> i.getAge())
+                .toList();
     }
 
     public static List<Student> getStudentsWithMinimumAge(List<Student> students, int minAge) {
-        return new ArrayList<>();
+        return students.stream().filter(student -> student.getAge() <= minAge).toList();
     }
 
 
     // getGender() == Gender.MALE
     // or getGender().name().equals("MALE")
     public static long countMaleStudents(List<Student> students) {
-        return 0;
+        return students.stream().filter(student -> student.getGender() == Gender.MALE).toList().size();
     }
 
 
     public static double avgAgeOfFemaleStudent(List<Student> students) {
-        return 0.0;
+        return students.stream().filter(student -> student.getGender() == Gender.FEMALE).collect(Collectors.averagingDouble(student -> student.getAge()));
     }
 
     public static Integer getProductOfStudentAges(List<Student> students) {
-        return 0;
+        return students.stream().map(student -> student.getAge()).reduce(1, (acc, i) -> acc*i);
     }
 
     // ignore F Grades
     public static double productOfStudentGrades(Student student) {
-        return 0.0;
+        return student.getGrades().stream().filter(grade -> grade.getType() != GradeType.F).map(grade -> grade.getType().getValue()).reduce(1, (acc, grade) -> acc * grade);
     }
 
     // region BONUS
